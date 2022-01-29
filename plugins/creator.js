61f1412b36666b51fc7af478b0297ecc404db72c
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-29 20:21:09 +0700

    Delete plugins directory

--- plugins/creator.js
+++
@@ -1,8 +0,0 @@
-function handler(m) {
-  this.sendContact(m.chat, '6282245409072', 'Robby Catur', m)
-}
-handler.help = ['creator', 'owner']
-handler.tags = ['info']
-handler.command = /^(creator|owner)$/i
-
-module.exports = handler
--- plugins/quotesimage.js
+++
@@ -1,6 +0,0 @@
-let handler = async (m, { conn }) => {
-	m.reply('Proses')
-  conn.sendFile(m.chat, `https://docs-jojo.herokuapp.com/api/quotes-image`, 'quotes.jpg', `Quotes image © Robby Catur`, m)
-  }
-  handler.command = /^q(uotes)?(image|img)$/i
-  module.exports = handler
